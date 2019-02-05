b73afe9148e973b8493c1611927fac9ece5ef18d
luis chagas <luischagas78@gmail.com>
2019-02-05 12:38:13 -0500

    Not quite the result I wanted until now

--- catawiki_best_deals_scraper.rb
+++ catawiki_best_deals_scraper.rb
@@ -1,6 +1,17 @@
 require 'open-uri'
 require 'nokogiri'
 require 'json'
+require 'date'
+
+@dtoday = Date.today.strftime("%Y%m%d")
+puts @dtoday
+puts @dtoday.class
+
+@dtomorrow = (@dtoday.to_i + 1).to_s
+puts @dtomorrow
+puts @dtomorrow.class
+
+@x = 0
 
 categories = [
   # "1-antiques-curio?927=62024&reserve_price=0",
@@ -20,45 +31,65 @@ categories = [
   # "1097-sports?927=62024&reserve_price=0",
   # "363-toys-models?927=62024&reserve_price=0",
   # "720-wine-whisky?927=62024&reserve_price=0"
-  ]
+]
 
 filtered_urls = []
+
+puts "The following auctions:"
+puts " - have no reserve price"
+puts " - will end in less than one day"
+puts " - have an estimated price from a Catawiki specialist"
+puts "   |    |    |    |    | "
+puts "   V    V    V    V    V "
 
 categories.each do |category_url|
   mother_url = "https://www.catawiki.com/c"
   child_url = category_url
-  url = "#{mother_url}/#{child_url}"
-  html_file = open(url).read
-  sleep(1)
-  html_doc = Nokogiri::HTML(html_file)
-  sleep(1)
+  url = "#{mother_url}/#{child_url}&bidding_end_days=#{@dtoday},#{@dtomorrow}"
+  html_search_file = open(url).read
+  html_search_doc = Nokogiri::HTML(html_search_file)
 
-  html_doc.search(".c-card").each do |element|
-    filtered_urls << element.attribute('href').to_s
+  html_search_doc.search(".c-card").each do |lot|
+    filtered_urls << lot.attribute("href").to_s
   end
 
-  puts "The following auctions have no reserve price:"
+  puts filtered_urls
 
-  filtered_urls.each do |filtered_url|
-    html_file = open(filtered_url).read
-    html_doc = Nokogiri::HTML(html_file)
+  html_search_doc.search(".c-card__title be-lot__title") do |auction|
+    @title = auction.text.to_s
+  end
 
-    html_doc.search(".cw-currency-amount-eur").each do |estimated_price|
-      cost_interval = estimated_price.text
-      @value = cost_interval.match(/([0-9]+,[0-9]{3})|([0-9]{3})/)
-      # puts value
-    end
+  puts @title
+end
 
-    html_doc.search(".amount").each do |bid|
-      # puts bid.text
-      current_bid_value = bid.text
-      # puts current_bid_value
-      @current_bid = current_bid_value.match(/([0-9]+,[0-9]{3})|([0-9]{3})/)
-      # puts current_bid
-    end
+filtered_urls.each do |filtered_url|
+  html_auction_file = open(filtered_url).read
+  html_auction_doc = Nokogiri::HTML(html_auction_file)
 
-    if html_doc.at_css(".cw-currency-amount-eur")
-      puts " - value: #{@value} | next minimum bid: #{@current_bid}"
-    end
+  html_auction_doc.search(".cw-currency-amount-eur").each do |estimated_price|
+    cost_interval = estimated_price.text
+    @value = cost_interval.match(/([0-9]+,[0-9]{3})|([0-9]{3})/)
+  end
+
+  html_auction_doc.search(".amount").each do |bid|
+    current_bid_value = bid.text
+    @current_bid = current_bid_value.match(/([0-9]+,[0-9]{3})|([0-9]{3})/)
+  end
+
+  # html_auction_doc.search(".lot_title") do |object|
+  #   @title = object.attribute("title")
+  # end
+
+  html_auction_doc.search(".counter cw-tab-fig hasCountdown").each do |time|
+    @end_time = time.text.to_s
+    @remaining_time = @end_time.match(/(\S+)(?=[h])/)
+  end
+
+  if html_auction_doc.at_css(".cw-currency-amount-eur")
+    puts "#{@x += 1} title: #{@title}"
+    puts " - value: #{@value} | next minimum bid: #{@current_bid}"
+    puts "   > ends in #{@remaining_time}"
+    puts "#{@end_time}"
+    puts "- - - - - -"
   end
 end

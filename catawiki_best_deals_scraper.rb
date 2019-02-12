a8b4063fe1d6d9c3a26fb7ab66b64a82c884a805
luis chagas <luischagas78@gmail.com>
2019-02-12 07:07:19 -0500

    now I have an array with the URLs of the auctions I wanted, and also the next minimum bid for those auctions

--- catawiki_best_deals_scraper.rb
+++ catawiki_best_deals_scraper.rb
@@ -1,48 +1,28 @@
-require "open-uri"
-require "nokogiri"
+require 'open-uri'
+require 'nokogiri'
 
 # url = "https://www.catawiki.com/c/708-classic-cars-motorcycles-automobilia"
-url = "https://www.catawiki.com/c/708-classic-cars-motorcycles-automobilia?927=62024&reserve_price=0"
+url = 'https://www.catawiki.com/c/708-classic-cars-motorcycles-automobilia?927=62024&reserve_price=0'
 html = open(url)
 content = html.read
 parsed_content = Nokogiri::HTML(content)
-auctions = parsed_content.css(".be-lot__description")
-# puts auctions.class.instance_methods
-# puts auctions
+auctions = parsed_content.css('.be-lot__description')
 
-links = auctions.css('div.be-lot__description p').map { |link| link['a'] }
-puts links
+anchors = auctions.css('a[href]')
+links = anchors.map do |element|
+  element['href']
+end
 
-# urls = []
+# puts links
 
-# here I can explore all the interesting auctions from that url, and it works
-# all_auctions = doc.xpath('//article[@class="be-lot u-word-break"]')
-# puts all_auctions
+# everything works until here, with the last puts I get an array with all the URLs I wanted
 
-# puts doc.css("href")
+links.each do |auction_url|
+  html = open(auction_url)
+  content = html.read
+  parsed_content = Nokogiri::HTML(content)
+  next_minimum_bid = parsed_content.css(".amount")
+  puts next_minimum_bid
+end
 
-# div_map = div.map do |href|
-#   puts href.class
-# end
-
-# div_each = []
-# div.each do |auction|
-#   div_each << auction
-# end
-
-# all_auctions = div_each[0].to_a
-# puts all_auctions
-
-# musicians_starting_with_a = musicians.select do |musician|
-#   musician.start_with?("A")
-# end
-
-# doc.search('.href').each do |element|
-#   puts element
-# end
-
-# puts doc
-
-# doc.search('.be-lot u-word-break').each do |element|
-#   puts element
-# end
+# now I have the next minimum bid for all those auctions

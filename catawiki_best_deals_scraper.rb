1495687ff17d5ca9dfcfda95309ebbf33fba84c3
luis chagas <luischagas78@gmail.com>
2019-02-13 09:27:41 -0500

    now I have the title, url, expert's estimate value, next minimum bid # and location for all the auctions I want

--- catawiki_best_deals_scraper.rb
+++ catawiki_best_deals_scraper.rb
@@ -15,14 +15,27 @@ end
 
 # puts links
 
-# everything works until here, with the last puts I get an array with all the URLs I wanted
+# everything works until here, with the last puts I get an array
+# with all the URLs I wanted
+# - - - - - - - - - - - - -
 
-links.each do |auction_url|
-  html = open(auction_url)
+links.each do |auction|
+  html = open(auction)
   content = html.read
   parsed_content = Nokogiri::HTML(content)
-  next_minimum_bid = parsed_content.css(".amount")
-  puts next_minimum_bid
+  title = parsed_content.css('.lot_title').text
+  puts "-> Auction title: #{title}"
+  auction_url = auction
+  puts "URL: #{auction_url}"
+  estimate_value = parsed_content.css('.cw-currency-amount-eur').text.split(' ')
+  puts "Expert's estimate: #{estimate_value[0]} - #{estimate_value[2]}"
+  next_minimum_bid = parsed_content.css('.amount').text
+  puts "Next minimum bid is: #{next_minimum_bid}"
+  location = parsed_content.css('.bid_extra_costs').text.split(' ')
+  puts "#{location[0]} #{location[1]}"
+  puts " "
 end
 
-# now I have the next minimum bid for all those auctions
+# now I have the title, url, expert's estimate value, next minimum bid
+# and location for all those auctions
+# - - - - - - - - - - - - -
